2fd96a48c5c7fadb5786987c000b52abd2b874e8
Ryo Ito <ryo.ito@livesense.co.jp>
2022-09-29 18:34:24 +0900

    SQLインジェクションの説明用メソッドを実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  class << self
+    def where_name_like(freeword)
+      where("name LIKE ?", "%#{freeword}%")
+    end
+
+    def where_name_like_with_security_risk(freeword)
+      where("name LIKE '%#{freeword}%'")
+    end
+  end
 end
