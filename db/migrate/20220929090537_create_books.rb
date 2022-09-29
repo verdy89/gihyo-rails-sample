c206167bebd4cfa4d120f3abaa6472c4eed4a9b8
Ryo Ito <ryo.ito@livesense.co.jp>
2022-09-29 18:11:09 +0900

    Book モデルの作成

---
+++ db/migrate/20220929090537_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+
+    add_index :books, :name, unique: true
+  end
+end
